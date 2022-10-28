ece2b54f8214d1862840f911bdbecf8edc13c72c
Jack Chan <awcjack@gmail.com>
2022-10-28 14:41:28 +0100

    chore: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"commonjs": true,
+		"es6": true
+	},
+	"parser": "@typescript-eslint/parser",
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended"
+	]
+}
